1a092d5f9bf2ba2a17d7a79364cc90e745dc415b
abduallahnoorsayket <abdullahnoorsayket@gmail.com>
2020-07-13 10:50:07 +0600

    pagination & user_posts.html added

--- Blog/urls.py
+++ Blog/urls.py
@@ -7,7 +7,8 @@ from .views import (
             PostDetailView,
             PostCreateView,
             PostUpdateView,
-            PostDeleteView
+            PostDeleteView,
+            UserPostListView
 
             )
 from . import views
@@ -16,6 +17,7 @@ urlpatterns = [
     # path('',views.home , name='blog-home'),
     path('',PostListView.as_view() , name='blog-home'),
     path('post/<int:pk>',PostDetailView.as_view() , name='post-detail'),
+    path('user/<str:username>',UserPostListView.as_view() , name='user-posts'),
     path('post/<int:pk>/update/',PostUpdateView.as_view() , name='post-update'),
     path('post/<int:pk>/delete/',PostDeleteView.as_view() , name='post-delete'),
     path('post/new/',PostCreateView.as_view() , name='post-create'),
--- Blog/views.py
+++ Blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView,
     DetailView,
@@ -23,6 +24,18 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
@@ -40,7 +53,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
     template_name = "blog/post_form.html"
@@ -50,22 +63,20 @@ class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
         return super().form_valid(form)
 
     def test_func(self):
-        post =  self.get_object()
+        post = self.get_object()
         if self.request.user == post.author:
             return True
         else:
             return False
 
 
-
-
-class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = "blog/post_confirm_delete.html"
     success_url = "/"
 
     def test_func(self):
-        post =  self.get_object()
+        post = self.get_object()
         if self.request.user == post.author:
             return True
         else:
